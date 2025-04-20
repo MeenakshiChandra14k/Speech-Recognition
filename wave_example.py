f31429bdbf22eefc79df4a9d3d314ecea3ab7cac
Sajith CS <sajith@Sajiths-MacBook-Pro.local>
2025-04-20 14:57:28 +0530

    Stage and commit all changes including new wav file

--- wave_example.py
+++ wave_example.py
@@ -1,1 +1,16 @@
 import wave
+
+obj = wave.open("harvard.wav", "rb")
+
+print("Number of channels: ", obj.getnchannels())
+print("Sample width: ", obj.getsampwidth())
+print("Frame rate: ", obj.getframerate())
+print("Number of frames: ", obj.getnframes())
+print("Parameters: ", obj.getparams())
+
+time_audio = obj.getnframes()/ obj.getframerate()
+print(time_audio)
+
+frames = obj.readframes(-1) #read all frames
+print(type(frames), type(frames[0]))
+print(len(frames)/4) #(nchannels+samplewidth)= 2+2=4
